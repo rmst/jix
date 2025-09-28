e1d9e755da62af34e14f8fbfeec3c7ea7bbc3d56
Simon Ramstedt <simonramstedt@gmail.com>
2025-09-28 21:43:49 +0200

    fix: minor fix in nux-cli init

--- src/nux-cli/init/index.js
+++ src/nux-cli/init/index.js
@@ -10,6 +10,6 @@ export default () => {
 	let wd = process.cwd()
 	installJsConfig(wd)  // TODO: this function seems way to complicated
 	sh`mkdir -p '${wd}/nux_modules'`
-	sh`grep '^nux_modules$' .gitignore >/dev/null 2>&1 || echo 'nux_modules' >> .gitignore`
+	sh`grep '^nux_modules$' .gitignore >/dev/null 2>&1 || { echo; echo 'nux_modules'; } >> .gitignore`
 	sh`ln -sfn '${LOCAL_NUX_PATH}/nux/lib' '${wd}/nux_modules/nux'`
 }
