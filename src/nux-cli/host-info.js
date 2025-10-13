1731bb21adbd034963bc04189fb1b76b4572f6cf
Simon Ramstedt <simonramstedt@gmail.com>
2025-10-13 10:14:41 +0200

    feat(cli): host-info command prints effects

--- src/nux-cli/host-info.js
+++ src/nux-cli/host-info.js
@@ -1,5 +1,8 @@
 import { queryHostInfo, queryUserInfo } from './core/hosts.js'
 import { dedent } from '../nux/dedent.js'
+import db from './db/index.js'
+import set from './core/set.js'
+import { shortPath } from './effectUtil.js'
 
 export default {
 	name: 'host-info',
@@ -25,5 +28,35 @@ export default {
 		const user = a[1] || null
 		console.log(JSON.stringify(queryHostInfo(host, user), null, 2))
 		console.log(JSON.stringify(queryUserInfo(host, user), null, 2))
+
+		// Show applied effects for this target
+		if (db.active.exists()) {
+			let activeHashesById = db.active.read()
+			let activeHashes = set(Object.values(activeHashesById).flat()).list()
+
+			let effectsForTarget = activeHashes
+				.map(hash => {
+					let effectData = db.store.read(hash)
+					return { ...effectData, hash }
+				})
+				.filter(effectData => {
+					// Filter by target
+					let matchesHost = (host === null && !effectData.host) || effectData.host === host
+					let matchesUser = (user === null && !effectData.user) || effectData.user === user
+					return matchesHost && matchesUser
+				})
+
+			console.log(`\nApplied effects: ${effectsForTarget.length}`)
+
+			let effectsWithValidPaths = effectsForTarget.filter(effectData => effectData.path)
+
+			if (effectsWithValidPaths.length > 0) {
+				console.log('\nApplied effects with valid paths:')
+				effectsWithValidPaths.forEach(effectData => {
+					let path = shortPath(effectData.hash)
+					console.log(`${path}\t${effectData.path}`)
+				})
+			}
+		}
 	}
 }
