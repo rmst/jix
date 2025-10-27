29c63021c33c9372727dd688b6052f8da955ee5e
Simon Ramstedt <simonramstedt@gmail.com>
2025-10-27 20:41:33 +0100

    feat: Added withNixOptions context manager for jix.nix

--- src/jix/nix/index.js
+++ src/jix/nix/index.js
@@ -1,9 +1,35 @@
 import jix from "../base"
 import { Effect } from "../effect"
+import { createContext, useContext } from "../useContext"
 
 /**
- * @typedef {Record<string, Record<string, Effect>>} NixPkgs
- */
+	@typedef {Record<string, Record<string, Effect>>} NixPkgs
+*/
+
+/**
+	@typedef {Object} NixOptions
+	@property {string | null} [nixpkgsPath] - Path to nixpkgs (e.g. https://github.com/NixOS/nixpkgs/archive/COMMIT_HASH.tar.gz)
+	@property {Record<string, string>} [extraArgs] - Additional nix-build arguments
+*/
+
+const NIX_CONTEXT = createContext(/** @type {NixOptions} */({ nixpkgsPath: null, extraArgs: {} }))
+
+/**
+	@template T
+	@param {NixOptions} options - Nix build options
+	@param {() => T} fn - Function to execute with these options
+	@returns {T}
+*/
+export const withNixOptions = (options, fn) => {
+	return NIX_CONTEXT.provide(options, fn)
+}
+
+/**
+	@returns {NixOptions}
+*/
+const getNixOptions = () => {
+	return useContext(NIX_CONTEXT)
+}
 
 /*
 https://nixos.org/guides/nix-pills/11-garbage-collector.html
@@ -13,27 +39,33 @@ https://nixos.org/guides/nix-pills/11-garbage-collector.html
 */
 
 /**
- * 
- * @param {*} name e.g. git
- * @param {*} nixpkgsPath, e.g. https://github.com/NixOS/nixpkgs/archive/YOUR_COMMIT_HASH.tar.gz
- * @returns the outpath for a nix package
- */
-export const pkg = (name, nixpkgsPath=null) => {
+	@param {Object} params
+	@param {string} params.name - Package name (e.g. 'git')
+	@param {NixOptions} [params.options={}] - Nix build options (merged with context)
+	@returns {Effect} the outpath for a nix package
+*/
+export const pkg = ({name, options={}}) => {
 
 	let target = jix.target()
+
+	// Merge context options with provided options (provided options override context)
+	options = {...getNixOptions(), ...options}
 
 	let nixbuildPath = target.host.os == "nixos"
 		? "/run/current-system/sw/bin/nix-build"
 		: "/nix/var/nix/profiles/default/bin/nix-build"
 
-	
-	let nixbuildArgs = nixpkgsPath
-		? `-I nixpkgs='${nixpkgsPath}'`
+	let nixbuildArgs = options.nixpkgsPath
+		? `-I nixpkgs='${options.nixpkgsPath}'`
 		: `'<nixpkgs>'`  // uses whatever is the current channel (TODO: this isn't reproducible and won't even trigger a re-evaluation if nixpkgs is updated)
-	
+
+	// Add any extra args
+	let extraArgsStr = Object.entries(options.extraArgs || {})
+		.map(([k, v]) => `${k} ${v}`)
+		.join(' ')
 
 	let derivation = jix.build`
-		"${nixbuildPath}" ${nixbuildArgs} -A "${name}" --out-link $out
+		"${nixbuildPath}" ${nixbuildArgs} ${extraArgsStr} -A "${name}" --out-link $out
 	`
 
 	return derivation
@@ -42,11 +74,18 @@ export const pkg = (name, nixpkgsPath=null) => {
 
 /** @type {NixPkgs} */
 export const pkgs = new Proxy({}, {
+	/**
+		@param {any} _
+		@param {string} name
+	*/
 	get: (_, name) => {
 		return new Proxy({}, {
+			/**
+				@param {any} _
+				@param {string} bin
+			*/
 			get: (_, bin) => {
-				// TODO: maybe make nixpkgsPath controllable here via useContext
-				return jix.str`${pkg(name)}/bin/${bin}`
+				return jix.str`${pkg({name})}/bin/${bin}`
 			}
 		})
 	},
@@ -56,4 +95,5 @@ export const pkgs = new Proxy({}, {
 export default {
 	pkg,
 	pkgs,
+	withNixOptions,
 }
