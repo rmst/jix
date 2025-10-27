ae184a6082c2da74673f87c565aaca52e9b641dd
Simon Ramstedt <simonramstedt@gmail.com>
2025-10-27 13:47:46 +0100

    fix: minor import error in jix.service

--- src/jix/service/index.js
+++ src/jix/service/index.js
@@ -1,4 +1,4 @@
-import jix from "../base.js"
+import jix, { buildDir } from "../base.js"
 import stateDir from "../stateDir.js"
 import launchdService from './launchd.js'
 import systemdService from './systemd.js'
@@ -96,7 +96,7 @@ export default ({
 		`;
 
 		// this is just so MacOS shows a nice name in various UIs and not a hash
-		wrapperScript = jix.str`${jix.experimental.buildDir({[label]: wrapperScript })}/${label}`
+		wrapperScript = jix.str`${buildDir({[label]: wrapperScript })}/${label}`
 
 		const serviceImplementation = serviceImplementations[target.host.os]
 
