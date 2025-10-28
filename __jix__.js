271e0b40139e69d326925e27aef38beafbe645f6
Simon Ramstedt <simonramstedt@gmail.com>
2025-10-28 17:42:16 +0100

    docs: enable local preview via docker

--- __jix__.js
+++ __jix__.js
@@ -6,6 +6,7 @@ const { watchfs } = jix.experimental.shelltools
 export const run = {
 	default: "echo hello from jix",
 	watch: () => `${watchfs} -r --wait . make install`,
+	docs: `docker run --rm -v "$PWD/docs:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll bash -c "bundle install && jekyll serve --host 0.0.0.0"`,
 }
 
 export const install = [
