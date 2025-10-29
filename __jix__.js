d40d0d9599c21485cac2ca29165772a041d2d903
Simon Ramstedt <simonramstedt@gmail.com>
2025-10-29 21:55:29 +0100

    refactor: cleanup __jix__.js

--- __jix__.js
+++ __jix__.js
@@ -1,52 +1,26 @@
-
-
 const { watchfs } = jix.experimental.shelltools
-const { imageFromDockerfile, docker, aptInstall } = jix.container
 
 
-const docs = () => {
-	let debianSlim = () => imageFromDockerfile`
-		# bookworm-slim 2025-09-30
-		FROM debian@\
-		sha256:7e490910eea2861b9664577a96b54ce68ea3e02ce7f51d89cb0103a6f9c386e0
-	`
+// image will be cleaned up automatically when no longer used
+let jekyll = () => jix.container.imageFromDockerfile`
+	FROM jekyll/jekyll@\
+	sha256:400b8d1569f118bca8a3a09a25f32803b00a55d1ea241feaf5f904d66ca9c625
+`
 
-	let jekyll = () => imageFromDockerfile`
-		FROM ${debianSlim}
+export const run = {
 
-		# aptInstall just returns a "RUN apt ..." string for us
-		${aptInstall("make", "build-essential", "ruby", "ruby-dev")}
+	default: "echo Hello from Jix!",
 
-		RUN gem install bundler jekyll
+	installAndWatch: () => `${watchfs} -r --wait . make install`,
 
-		RUN addgroup --system jekyll && adduser --system --group jekyll
-		USER jekyll
-		WORKDIR /srv/jekyll
-	`
-
-	// in this case we could also just use the pre-built jekyll image but our custom image is a good jix demo and allows for more control
-	let jekyll2 = () => imageFromDockerfile`
-		FROM jekyll/jekyll@sha256:400b8d1569f118bca8a3a09a25f32803b00a55d1ea241feaf5f904d66ca9c625
-	`
-
-	return jix.script`
-		${docker} run -it --rm \
+	docs: () => jix.script`
+		${jix.container.docker} run -it --rm \
 			-v "$PWD/docs:/srv/jekyll" \
 			-e BUNDLE_PATH=/srv/jekyll/vendor/bundle \
 			-p 4000:4000 \
 			${jekyll} \
 			bash -c "bundle install && bundle exec jekyll serve --host 0.0.0.0"
-	`
-}
-
-
-export const run = {
-
-	default: "echo hello from jix",
-
-	"watch-install": () => `${watchfs} -r --wait . make install`,
-
-	docs,
+	`,
 }
 
 
