db4c6f3bd4aa7cf0f047d103d9726817f00f0061
Simon Ramstedt <simonramstedt@gmail.com>
2025-11-05 11:21:29 +0100

    test: Cleaned up outdated tests

--- test/readline/runner_getkey.js
+++
@@ -1,8 +0,0 @@
-import { getKey } from '../../src/jix-cli/readline.js'
-
-const main = async () => {
-	const k = await getKey()
-	console.log(`RESULT:${k}`)
-}
-
-main()
--- test/readline/runner_prompt.js
+++
@@ -1,8 +0,0 @@
-import { promptLine } from '../../src/jix-cli/readline.js'
-
-const main = async () => {
-	const v = await promptLine('')
-	console.log(`RESULT:${v}`)
-}
-
-main()
